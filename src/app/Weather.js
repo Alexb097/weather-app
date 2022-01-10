f3756afef5927b3e06b4cffe26e10187d9cb45c2
Aleb097 <alex.bogdan056@gmail.com>
2022-01-10 20:12:24 -0300

    first commit

---
+++ src/app/Weather.js
@@ -0,0 +1,20 @@
+export class Weather {
+
+    constructor(city, countryCode){
+        this.apikey = '60083cba30afee19a4a3cd81a7471605';
+        this.city = city;
+        this.countryCode = countryCode;
+    }
+
+    async getWeather(){
+        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
+        const response = await fetch(URI);
+        const data = await response.json();
+        return data;
+    }
+
+    changeLocation(city, countryCode) {
+        this.city = city;
+        this.countryCode = countryCode;
+    }
+}
